018af2c851bab24298937fddd82a13734ec70082
cosmas Thuku <cosmasbachia@equitybank.co.ke>
2022-04-25 21:04:11 +0300

    express backend

---
+++ expressBackend/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db:{
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"dummydata",
+    },
+    listPerPage:10,
+};
+module.exports = config;
